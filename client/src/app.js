32d0190cd95c89c4b2ab81d247ca723f25db6259
Scott Osman <codecso@protonmail.ch>
2019-01-16 14:07:00 +0000

    Added to Index.html

---
+++ client/src/app.js
@@ -0,0 +1,8 @@
+const GoalFormView = require('./views/goal_form_view.js');
+const GoalEditView = require('./views/goal_edit_view.js');
+const GoalGridView = require('./views/goal_grid_view.js');
+const Goals = require('./models/goals.js');
+
+document.addEventListener('DOMContentLoaded', () => {
+
+});
