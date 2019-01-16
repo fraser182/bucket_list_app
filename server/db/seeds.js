e4467542f96eb9370c4e6dddf87e1ba800f7dfb8
Scott Osman <codecso@protonmail.ch>
2019-01-16 15:41:53 +0000

    Fixed missing commas

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -7,25 +7,24 @@ db.goals.insertMany([
     description: "Learn tango, ride horses and eat local food",
     completeBy: "2020-09-01",
     completeStatus: "No"
-  }
+  },
   {
     title: "Get a Job as a Software Developer",
     description: "Aim to work in Silicon Valley for Apple.",
     completeBy: "2030-06-15",
     completeStatus: "No"
-  }
+  },
   {
     title: "Ride the Fastest Roller Coaster in the World",
     description: "Save money to buy plane tickets and have some spending money. Contact Steven to arrange dates.",
     completeBy: "2020-04-5",
     completeStatus: "No"
-  }
+  },
   {
     title: "Make a Billion Dollar Company",
     description: "Employ all of CodeClan G10 and aim to take over the world.",
     completeBy: "2019-04-01",
     completeStatus: "No"
   }
-
 
 ]);
